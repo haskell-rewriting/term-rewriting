ac058b3281473cdeeb55e931fef4c8cb42953c09
Bertram Felgenhauer <int-e@gmx.de>
2011-03-02 11:42:52 +0100

    implement apply, apply', match

--- src/Data/Rewriting/Substitution/Type.hs
+++ src/Data/Rewriting/Substitution/Type.hs
@@ -1,11 +1,12 @@
 module Data.Rewriting.Substitution.Type (
     Substitution,
     Substitution',
+    module Data.Rewriting.Term.Type,
 ) where
 
-import Data.Rewriting.Term
+import Data.Rewriting.Term.Type
 import qualified Data.Map as M
 
 type Substitution f v = M.Map v (Term f v)
 
-type Substitution' v' f v = M.Map v' (Term f v)
+type Substitution' v f v' = M.Map v (Term f v')
