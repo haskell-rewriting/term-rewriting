f2f4fde83963d09c01c7df75ba50d029080fe6e7
griff <c.sternagel@gmail.com>
2011-03-02 17:06:45 +0100

    proper haddock syntax for Parse.hs documentation

--- src/Data/Rewriting/Term/Parse.hs
+++ src/Data/Rewriting/Term/Parse.hs
@@ -12,16 +12,15 @@ import Data.Char
 import Text.Parsec hiding (parse)
 import qualified Text.Parsec as Parsec
 
-{- 'fromString xs s' parsers a term from the string 's', where elements of 'xs'
-   are considered as variables.
+{-| @fromString xs s@ parsers a term from the string @s@, where elements of @xs@
+are considered as variables.
 
-   > fromString ["x","y"] "f(x,c)"
+> > fromString ["x","y"] "f(x,c)"
 
-   results in
+results in
 
-   Fun "f" [Var "x",Fun "c" []]
-
- -}
+> Fun "f" [Var "x",Fun "c" []]
+-}
 fromString :: [String] -> String -> IO (Term String String)
 fromString xs input = case Parsec.parse (all $ parseWST xs) "" input of
   Left err -> do { putStr "parse error at "; print err; mzero }
@@ -29,13 +28,13 @@ fromString xs input = case Parsec.parse (all $ parseWST xs) "" input of
   where
     all p = do { x <- p; spaces; eof; return x }
 
-{- A parser for terms, where 'funP' and 'varP' are parsers for function symbols
-   and variables, respectively. The 'varP' parser has a higher priority than the
-   'funP' parser. Hence, whenever 'varP' succeeds, the token is treated as a
-   variable.
+{-| A parser for terms, where @funP@ and @varP@ are parsers for function symbols
+and variables, respectively. The @varP@ parser has a higher priority than the
+@funP@ parser. Hence, whenever @varP@ succeeds, the token is treated as a
+variable.
 
-   Note that the user has to take care of handling trailing white space in
-   [funP] and [varP].
+Note that the user has to take care of handling trailing white space in
+@funP@ and @varP@.
  -}
 parse :: Stream s m Char => ParsecT s u m f -> ParsecT s u m v
   -> ParsecT s u m (Term f v)
@@ -45,11 +44,11 @@ parse funP varP = term <?> "term"
     args = between (lex $ char '(') (lex $ char ')') (sepBy term (lex $ char ','))
              <|> return []
 
-{- A parser for terms following the conventions of the ancient ASCII input
-   format for the termination competition: every 'Char' that is neither a white
-   space (according to 'Data.Char.isSpace') nor one of ''('', '')'', or '','',
-   is considered a letter. An identifier is a non-empty sequence of letters and
-   it is treated as variable iff it is contained in 'xs'.
+{-| A parser for terms following the conventions of the ancient ASCII input
+format for the termination competition: every @Char@ that is neither a white
+space (according to @Data.Char.isSpace@) nor one of @'('@, @')'@, or @','@,
+is considered a letter. An identifier is a non-empty sequence of letters and
+it is treated as variable iff it is contained in @xs@.
  -}
 -- change name?
 parseWST :: Stream s m Char => [String] -> ParsecT s u m (Term String String)
@@ -59,5 +58,5 @@ parseWST xs = parse funP varP
     varP  = do { x <- lex ident; if x `elem` xs then return x else mzero }
     ident = many1 (satisfy (\c -> not (isSpace c) && not (c `elem` "(),")))
 
-{- Same as 'p' but also consume trailing white space. -}
+{- Same as @p@ but also consume trailing white space. -}
 lex p = do { x <- p; spaces; return x }
