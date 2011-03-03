cedd8a9c7241ece2ea30395cbe049f050a0cb33f
griff <c.sternagel@gmail.com>
2011-03-03 12:08:23 +0100

    removed too specific function

--- src/Data/Rewriting/Utils.hs
+++ src/Data/Rewriting/Utils.hs
@@ -1,10 +1,2 @@
-{-# OPTIONS_GHC -XFlexibleContexts#-}
 module Data.Rewriting.Utils (
-  parseFromString
 ) where
-
-import Text.Parsec
-
-parseFromString :: Parsec String () a -> String -> Either ParseError a
-parseFromString p = parse (all p) ""
-  where all = between spaces (spaces >> eof)
