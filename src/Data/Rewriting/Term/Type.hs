769595a5cd8148285a7241cbb0f2c221271f7e9b
Martin <martin.avanzini@uibk.ac.at>
2011-03-03 09:12:29 +0100

    change indentation

--- src/Data/Rewriting/Term/Type.hs
+++ src/Data/Rewriting/Term/Type.hs
@@ -7,8 +7,8 @@ module Data.Rewriting.Term.Type (
 data Term f v = Var v | Fun f [Term f v]
     deriving (Show, Eq, Ord)
 
-foldTerm:: (v -> a) -> (f -> [a] -> a) -> Term f v -> a
-foldTerm var fun (Var v) = var v
+foldTerm :: (v -> a) -> (f -> [a] -> a) -> Term f v -> a
+foldTerm var fun (Var v)    = var v
 foldTerm var fun (Fun f ts) = fun f (map (foldTerm var fun) ts)
 
 mapTerm :: (v -> v') -> (f -> f') -> Term f v -> Term f' v'
