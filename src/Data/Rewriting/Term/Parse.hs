101c6900a70dfaca31815673050de7e3ad0f2412
griff <c.sternagel@gmail.com>
2011-03-03 12:48:18 +0100

    changed documentation style

--- src/Data/Rewriting/Term/Parse.hs
+++ src/Data/Rewriting/Term/Parse.hs
@@ -17,31 +17,29 @@ import Data.Char
 import Text.Parsec hiding (parse)
 import qualified Text.Parsec as Parsec
 
-{-| Like @fromString@, but the result is wrapped in the IO monad, making this
-function useful for interactive testing.
-
->>> parseIO ["x","y"] "f(x,c)"
-Fun "f" [Var "x",Fun "c" []]
--}
+-- | Like @fromString@, but the result is wrapped in the IO monad, making this
+-- function useful for interactive testing.
+--
+-- >>> parseIO ["x","y"] "f(x,c)"
+-- Fun "f" [Var "x",Fun "c" []]
 parseIO :: [String] -> String -> IO (Term String String)
 parseIO xs input = case fromString xs input of
   Left err -> do { putStr "parse error at "; print err; mzero }
   Right t  -> return t
 
-{-| @fromString xs s@ parsers a term from the string @s@, where elements of @xs@
-are considered as variables. -}
+-- | @fromString xs s@ parsers a term from the string @s@, where elements of @xs@
+-- are considered as variables.
 fromString :: [String] -> String -> Either ParseError (Term String String)
 fromString xs = runP (parseWST xs) () ""
 
 
-{-| A parser for terms, where @funP@ and @varP@ are parsers for function symbols
-and variables, respectively. The @varP@ parser has a higher priority than the
-@funP@ parser. Hence, whenever @varP@ succeeds, the token is treated as a
-variable.
-
-Note that the user has to take care of handling trailing white space in
-@funP@ and @varP@.
- -}
+-- | A parser for terms, where @funP@ and @varP@ are parsers for function symbols
+-- and variables, respectively. The @varP@ parser has a higher priority than the
+-- @funP@ parser. Hence, whenever @varP@ succeeds, the token is treated as a
+-- variable.
+-- 
+-- Note that the user has to take care of handling trailing white space in
+-- @funP@ and @varP@.
 parse :: Stream s m Char => ParsecT s u m f -> ParsecT s u m v
   -> ParsecT s u m (Term f v)
 parse funP varP = term <?> "term"
@@ -51,12 +49,12 @@ parse funP varP = term <?> "term"
              <|> return []
 
 
-{-| A parser for terms following the conventions of the ancient ASCII input
-format for the termination competition: every @Char@ that is neither a white
-space (according to @Data.Char.isSpace@) nor one of @'('@, @')'@, or @','@,
-is considered a letter. An identifier is a non-empty sequence of letters and
-it is treated as variable iff it is contained in @xs@.
- -}
+-- | A parser for terms following the conventions of the ancient ASCII input
+-- format for the termination competition: every @Char@ that is neither a white
+-- space (according to @Data.Char.isSpace@) nor one of @'('@, @')'@, or @','@,
+-- is considered a letter. An identifier is a non-empty sequence of letters and
+-- it is treated as variable iff it is contained in @xs@.
+
 -- change name?
 parseWST :: Stream s m Char => [String] -> ParsecT s u m (Term String String)
 parseWST xs = parse parseFunWST (parseVarWST xs)
@@ -72,5 +70,5 @@ parseVarWST xs =
 ident :: Stream s m Char => ParsecT s u m String
 ident = many1 (satisfy (\c -> not (isSpace c) && not (c `elem` "(),")))
 
-{- Same as @p@ but also consume trailing white space. -}
+-- Same as @p@ but also consume trailing white space.
 lex p = do { x <- p; spaces; return x }
