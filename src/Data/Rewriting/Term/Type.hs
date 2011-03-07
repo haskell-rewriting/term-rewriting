4cd227235838e441610dc2aa6743c2b0e09b1149
Martin <martin.avanzini@uibk.ac.at>
2011-03-07 13:35:27 +0100

    pretty printing for terms, rules and substitutions implemented
    - for types T, added prettyprinter prettyT parameterised in prettyprinters for variables and function symbols
    - added instance declarations for class Pretty

--- src/Data/Rewriting/Term/Type.hs
+++ src/Data/Rewriting/Term/Type.hs
@@ -2,9 +2,11 @@ module Data.Rewriting.Term.Type (
     Term (..),
     fold,
     map,
+    prettyTerm
 ) where
 
 import Prelude hiding (map)
+import Text.PrettyPrint.ANSI.Leijen ((<>), encloseSep, lparen, rparen, comma, Pretty(..), Doc)
 
 data Term f v
     = Var v            -- ^ Variable
@@ -25,4 +27,13 @@ fold var fun (Fun f ts) = fun f (fmap (fold var fun) ts)
 -- Fun 'e' [Var 3,Fun 'f' []]
 map :: (v -> v') -> (f -> f') -> Term f v -> Term f' v'
 map var fun = fold (Var . var) (Fun . fun)
+
+prettyTerm :: (f -> Doc) -> (v -> Doc) -> Term f v -> Doc
+prettyTerm _          prettyVars (Var x)    = prettyVars x
+prettyTerm prettyFuns prettyVars (Fun f ts) = prettyFuns f <> prettyArgs
+    where prettyArgs = encloseSep lparen rparen comma [prettyTerm prettyFuns prettyVars ti | ti <- ts]
+
+instance (Pretty f, Pretty v) => Pretty (Term f v) where
+    pretty = prettyTerm pretty pretty
+
 
