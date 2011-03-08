763d08c865906826b80dd5ef611e41965c6dd524
Bertram Felgenhauer <int-e@gmx.de>
2011-03-08 18:10:13 +0100

    don't export Term.map/fold from Rule; derive Ord/Eq for Rule

--- src/Data/Rewriting/Rule/Type.hs
+++ src/Data/Rewriting/Rule/Type.hs
@@ -3,10 +3,11 @@ module Data.Rewriting.Rule.Type (
     Rule (..),
 ) where
 
-import Data.Rewriting.Term.Type
+import Data.Rewriting.Term.Type hiding (map, fold)
 
 -- | Rewrite rule with left-hand side and right-hand side.
 data Rule f v = Rule { lhs :: Term f v, rhs :: Term f v }
+    deriving (Ord, Eq)
 
 -- mapRule :: (Term f v -> Term f' v') -> Rule f v -> Rule f' v'
 -- mapRule f r = Rule{ lhs = f (lhs r), rhs = f (rhs r) }
