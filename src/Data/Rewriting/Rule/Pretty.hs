8b0e1585ad00a474793b0e03ac4ca08d542bb4f0
Martin Avanzini <zini@gaming.lan>
2011-03-09 17:43:41 +0100

    prettyRule parameterised in printer for arrow

--- src/Data/Rewriting/Rule/Pretty.hs
+++ src/Data/Rewriting/Rule/Pretty.hs
@@ -7,10 +7,10 @@ import Data.Rewriting.Term (prettyTerm)
 
 import Text.PrettyPrint.ANSI.Leijen
 
-prettyRule :: (f -> Doc) -> (v -> Doc) -> Rule f v -> Doc
-prettyRule fun var (Rule l r) = term l </> text "->" <+> term r where
+prettyRule :: Doc -> (f -> Doc) -> (v -> Doc) -> Rule f v -> Doc
+prettyRule arr fun var (Rule l r) = term l </> arr <+> term r where
     term = prettyTerm fun var
 
 instance (Pretty f, Pretty v) => Pretty (Rule f v) where
-    pretty = prettyRule pretty pretty
+    pretty = prettyRule (text "->") pretty pretty
 
