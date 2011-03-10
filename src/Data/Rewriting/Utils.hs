0bf5a9834639e6c07b743099ef0e1bc0338a995f
griff <c.sternagel@gmail.com>
2011-03-10 10:44:46 +0100

    added auxiliary function

--- src/Data/Rewriting/Utils.hs
+++ src/Data/Rewriting/Utils.hs
@@ -1,2 +1,12 @@
 module Data.Rewriting.Utils (
+    dropCommonPrefix,
 ) where
+
+-- | @dropCommonPrefix xs ys@ removes the common prefix of @xs@ and @ys@ and
+-- returns the remaining lists as a pair.
+--
+-- >>>dropCommonPrefix [1,2,3] [1,2,4,1]
+-- ([3], [4,1])
+dropCommonPrefix :: Ord a => [a] -> [a] -> ([a], [a])
+dropCommonPrefix (x:xs) (y:ys) | x == y = dropCommonPrefix xs ys
+dropCommonPrefix   xs     ys            = (xs, ys)
