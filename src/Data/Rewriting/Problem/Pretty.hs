2544cb91118a178c1e0d310a104c81ea5d8d2481
Martin Avanzini <martin.avanzini@uibk.ac.at>
2011-03-13 18:44:42 +0100

    pretty printing of rules and problems improved

--- src/Data/Rewriting/Problem/Pretty.hs
+++ src/Data/Rewriting/Problem/Pretty.hs
@@ -3,20 +3,21 @@ module Data.Rewriting.Problem.Pretty (
 ) where                        
 
 import Data.Maybe (isJust)
+import Data.List (nub)
 import Data.Rewriting.Problem.Type
 import Data.Rewriting.Rule (prettyRule)
 import Text.PrettyPrint.ANSI.Leijen
 
-prettyProblem :: (f -> Doc) -> (v -> Doc) -> Problem f v -> Doc
-prettyProblem fun var prob =  block "Start terms" (ppST `on` startTerms) 
-                              <$> block "Strategy" (ppStrat `on` strategy) 
-                              <$> block "Variables" (ppVars `on` variables) 
-                              <$> block "Function Symbols" (ppSyms `on` symbols) 
-                              <$> printWhen (isJust `on` comment) (block "Comment" (ppComment `on` comment)) 
-                              <$> block "Rules" (ppRules `on` rules) where
+prettyProblem :: (Eq f, Eq v) => (f -> Doc) -> (v -> Doc) -> Problem f v -> Doc
+prettyProblem fun var prob =  block "Start-Terms" (ppST `on` startTerms) 
+                              <$$> block "Strategy" (ppStrat `on` strategy) 
+                              <$$> block "Variables" (ppVars `on` variables) 
+                              <$$> block "Function Symbols" (ppSyms `on` symbols) 
+--                              <$$> printWhen (isJust `on` comment) (block "Comment" (ppComment `on` comment)) 
+                              <$$> block "Rules" (ppRules `on` rules) where
   pp `on` fld = pp $ fld prob
-  block n pp = fillBreak 15 (text $ n ++ ":") <+> pp
-  commalist  = encloseSep empty empty comma
+  block n pp = hang 3 $ (underline $ text $ n ++ ":") <+> pp
+  commalist  = fillSep . punctuate (text ",")
   
   printWhen False _ = empty
   printWhen True  p = p
@@ -26,13 +27,13 @@ prettyProblem fun var prob =  block "Start terms" (ppST `on` startTerms)
   ppStrat Innermost  = text "innermost"
   ppStrat Outermost  = text "outermost"
   ppStrat Full       = text "full rewriting"
-  ppVars vars        = commalist [var v | v <- vars]
-  ppSyms syms        = commalist [fun v | v <- syms]
+  ppVars vars        = commalist $ [var v | v <- nub vars]
+  ppSyms syms        = commalist $ [fun v | v <- nub syms]
   ppComment (Just c) = text c
   ppComment Nothing  = empty
-  ppRules rp         = semiBraces $ 
-                       [prettyRule (text "->") fun var r | r <- strictRules rp] 
-                       ++ [prettyRule (text "->=") fun var r | r <- weakRules rp]
-                       
-instance (Pretty f, Pretty v) => Pretty (Problem f v) where
+  ppRules rp         = braces $ align $ vcat $
+                       [ppRule "->" r | r <- strictRules rp] 
+                       ++ [ppRule "->=" r | r <- weakRules rp]
+  ppRule sep         = prettyRule (text sep) fun var
+instance (Eq f, Eq v, Pretty f, Pretty v) => Pretty (Problem f v) where
   pretty = prettyProblem pretty pretty
--- src/Data/Rewriting/Rule/Pretty.hs
+++ src/Data/Rewriting/Rule/Pretty.hs
@@ -8,7 +8,7 @@ import Data.Rewriting.Term (prettyTerm)
 import Text.PrettyPrint.ANSI.Leijen
 
 prettyRule :: Doc -> (f -> Doc) -> (v -> Doc) -> Rule f v -> Doc
-prettyRule arr fun var (Rule l r) = term l </> arr <+> term r where
+prettyRule arr fun var (Rule l r) = hang 2 $ term l <+> arr </> term r where
     term = prettyTerm fun var
 
 instance (Pretty f, Pretty v) => Pretty (Rule f v) where
