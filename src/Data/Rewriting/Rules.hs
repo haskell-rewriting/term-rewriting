defa3055aa03dbd63dc36579a3067fd7c636a77b
Martin Avanzini <martin.avanzini@uibk.ac.at>
2011-03-13 18:04:35 +0100

    added "funsDL :: [Rule f v] -> [f] -> [f]"

--- src/Data/Rewriting/Rules.hs
+++ src/Data/Rewriting/Rules.hs
@@ -1,5 +1,11 @@
 module Data.Rewriting.Rules (
-    module Data.Rewriting.Rules.Rewrite
+    module Data.Rewriting.Rules.Rewrite, 
+    funsDL
 ) where
 
 import Data.Rewriting.Rules.Rewrite hiding (nested, listContexts)
+import Data.Rewriting.Rule (Rule)
+import qualified Data.Rewriting.Rule as Rule
+
+funsDL :: [Rule f v] -> [f] -> [f]
+funsDL rs fs = foldr Rule.funsDL fs rs
