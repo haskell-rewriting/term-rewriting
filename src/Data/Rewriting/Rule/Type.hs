89a71bb1fed8f4cec5d900316452524d531dff5b
Martin Avanzini <martin.avanzini@uibk.ac.at>
2011-03-13 18:01:14 +0100

    datatype Rule derives Show

--- src/Data/Rewriting/Rule/Type.hs
+++ src/Data/Rewriting/Rule/Type.hs
@@ -7,7 +7,7 @@ import Data.Rewriting.Term.Type hiding (map, fold)
 
 -- | Rewrite rule with left-hand side and right-hand side.
 data Rule f v = Rule { lhs :: Term f v, rhs :: Term f v }
-    deriving (Ord, Eq)
+    deriving (Ord, Eq, Show)
 
 -- mapRule :: (Term f v -> Term f' v') -> Rule f v -> Rule f' v'
 -- mapRule f r = Rule{ lhs = f (lhs r), rhs = f (rhs r) }
