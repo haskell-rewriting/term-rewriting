451b756fc7e24c0237c89765045a563dec9eddc7
griff <c.sternagel@gmail.com>
2011-03-16 17:25:05 +0100

    added contexts
     - maybe the constructor names could be improved
     - currently the list of terms left of the hole is NOT in reversed order; what
       would be the advantage of doing so?

---
+++ src/Data/Rewriting/Context/Ops.hs
@@ -0,0 +1,32 @@
+module Data.Rewriting.Context.Ops (
+    apply,
+    compose,
+    ofTerm,
+) where
+
+import Control.Monad
+import Data.Rewriting.Pos
+import Data.Rewriting.Term.Type
+import Data.Rewriting.Context.Type
+
+-- | Apply a context to a term (i.e., replace the hole in the context by the
+-- term).
+apply :: Ctxt f v -> Term f v -> Term f v
+apply Hole t = t
+apply (Ctxt f ts1 ctxt ts2) t = Fun f (ts1 ++ apply ctxt t : ts2)
+
+-- | Compose two contexts (i.e., replace the hole in the left context by the
+-- right context).
+compose :: Ctxt f v -> Ctxt f v -> Ctxt f v
+compose Hole c2 = c2
+compose (Ctxt f ts1 c1 ts2) c2 = Ctxt f ts1 (c1 `compose` c2) ts2
+
+-- | Create a context from a term by placing the hole at a specific position.
+ofTerm :: Term f v -> Pos -> Maybe (Ctxt f v)
+ofTerm _ [] = Just Hole
+ofTerm (Fun f ts) (i:p) = do
+    guard (i >= 0 && i < length ts)
+    let (ts1, t:ts2) = splitAt i ts
+    ctxt <- ofTerm t p
+    return (Ctxt f ts1 ctxt ts2)
+ofTerm _ _ = Nothing
