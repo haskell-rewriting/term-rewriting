cdfdda05dc2542183dfcb99e4d6535f030efb95a
Bertram Felgenhauer <int-e@gmx.de>
2011-03-16 19:32:26 +0100

    implement critical pairs
    Unlike discussed today, I return the original variables wrapped in an
    Either type. I believe my original reason against this was the resulting
    type's complexity, but using Ints is even more confusing.

---
+++ src/Data/Rewriting/CriticalPair/Ops.hs
@@ -0,0 +1,135 @@
+module Data.Rewriting.CriticalPair.Ops (
+    -- * pairs of rewrite systems
+    cps,
+    cpsIn,
+    cpsOut,
+    -- * single rewrite systems
+    cps',
+    cpsIn',
+    cpsOut',
+) where
+
+import Data.Rewriting.CriticalPair.Type
+import Data.Rewriting.Substitution
+import Data.Rewriting.Rule.Type
+import qualified Data.Rewriting.Term as Term
+import Data.Rewriting.Pos
+import Data.Rewriting.Rules.Rewrite (listContexts)
+
+import Data.Maybe
+import Control.Monad
+import Data.List
+
+-- cpW does all the hard work:
+-- Given a function that returns contexts to consider for rewriting,
+-- unify the contexts of the right rule's lhs with the left rule's lhs,
+-- and return the resulting critical pairs.
+cpW :: (Ord v, Ord v', Eq f)
+    => (Term f (Either v v')
+        -> [(Pos, Context f (Either v v'), Term f (Either v v'))])
+    -> Rule f v -> Rule f v' -> [(CP f v v')]
+cpW f rl rr = do
+    let rl' = Term.map Left id (lhs rl)
+        rr' = Term.map Right id (lhs rr)
+    (pos, ctx, rr'') <- f rr'
+    guard $ not (Term.isVar rr'')
+    subst <- maybeToList $ unify rl' rr''
+    return CP{
+        left = apply subst (ctx (Term.map Left id (rhs rl))),
+        top = apply subst rr',
+        right = apply subst (Term.map Right id (rhs rr)),
+        leftRule = rl,
+        leftPos = pos,
+        rightRule = rr,
+        subst = subst
+    }
+
+
+-- TODO: find a better place for this kind of contexts.
+type Context f v = Term f v -> Term f v
+
+-- Calculate contexts of a term, in pre-order.
+-- In particular, the root context is returned first.
+contexts :: Term f v -> [(Pos, Context f v, Term f v)]
+contexts t@(Var _)    = [([], id, t)]
+contexts t@(Fun f ts) = do
+    (i, ctxL, t) <- listContexts ts
+    (pos, ctxT, t') <- contexts t
+    return (i : pos, Fun f . ctxL . ctxT, t')
+
+-- Determine critical pairs for a pair of rules.
+cp :: (Ord v, Ord v', Eq f)
+    => Rule f v -> Rule f v' -> [(CP f v v')]
+cp  = cpW contexts
+
+-- Determine outer critical pairs for a pair of rules.
+cpOut :: (Ord v, Ord v', Eq f)
+    => Rule f v -> Rule f v' -> [(CP f v v')]
+cpOut = cpW (take 1 . contexts)
+
+-- Determine inner critical pairs for a pair of rules.
+cpIn :: (Ord v, Ord v', Eq f)
+    => Rule f v -> Rule f v' -> [(CP f v v')]
+cpIn = cpW (tail . contexts)
+
+
+-- | Determine all critical pairs for a pair of TRSs.
+cps :: (Ord v, Ord v', Eq f) => [Rule f v] -> [Rule f v']
+    -> [(CP f v v')]
+cps  trs1 trs2 = do
+    rl <- trs1
+    rr <- trs2
+    cp rl rr
+
+-- | Determine all inner critical pairs for a pair of TRSs.
+--
+-- A critical pair is /inner/ if the left rewrite step is not a root step.
+cpsIn :: (Ord v, Ord v', Eq f) => [Rule f v] -> [Rule f v']
+    -> [(CP f v v')]
+cpsIn trs1 trs2 = do
+    rl <- trs1
+    rr <- trs2
+    cpOut rl rr
+
+-- | Determine outer critical pairs for a pair of TRSs.
+--
+-- A critical pair is /outer/ if the left rewrite step is a root step.
+cpsOut :: (Ord v, Ord v', Eq f) => [Rule f v] -> [Rule f v']
+    -> [(CP f v v')]
+cpsOut trs1 trs2 = do
+    rl <- trs1
+    rr <- trs2
+    cpIn rl rr
+
+
+-- | Determine all critical pairs of a single TRS with itself.
+--
+-- Unlike @cps@, @cps'@ takes symmetries into account. See 'cpsIn'' and
+-- 'cpsOut'' for details.
+cps' :: (Ord v, Eq f) => [Rule f v] -> [(CP f v v)]
+cps'  trs = cpsIn' trs ++ cpsOut' trs
+
+-- | Determine all inner critical pairs of a single TRS with itself.
+--
+-- The result of @cpsIn' trs@ differs from @cpsIn trs trs@ in that overlaps
+-- of a rule with itself are returned once, not twice.
+cpsIn' :: (Ord v, Eq f) => [Rule f v] -> [(CP f v v)]
+cpsIn' trs = do
+    r1 : trs' <- tails trs
+    cpIn r1 r1 ++ do
+        r2 <- trs'
+        cpIn r1 r2 ++ cpIn r2 r1
+
+-- | Determine all outer critical pairs of a single TRS with itself.
+--
+-- The result of @cpsOut' trs@ differs from @cpsOut trs trs@ in two aspects:
+--
+--  * The trivial overlaps of rules with themselves are omitted.
+--
+--  * Symmetry is taken into account: Overlaps between distinct rules are
+--    returned once instead of twice.
+cpsOut' :: (Ord v, Eq f) => [Rule f v] -> [(CP f v v)]
+cpsOut' trs = do
+    rl : trs' <- tails trs
+    rr <- trs'
+    cpOut rl rr
---
+++ src/Data/Rewriting/CriticalPair/Type.hs
@@ -0,0 +1,28 @@
+module Data.Rewriting.CriticalPair.Type (
+    CP (..),
+) where
+
+import Data.Rewriting.Substitution
+import Data.Rewriting.Rule.Type
+import Data.Rewriting.Pos
+
+-- | A critical pair. Critical pairs (should) have the following properties:
+--
+-- @
+-- top   == Context.ofTerm top pos (Term.map Left id (Rule.lhs leftRule))
+-- left  == Context.ofTerm top pos (Term.map Left id (Rule.rhs leftRule))
+-- top   == Substitution.apply subst (Term.map Right id (Rule.lhs rightRule))
+-- right == Substitution.apply subst (Term.map Right id (Rule.rhs rightRule))
+-- @
+--
+-- Furthermore, @pos@ is a non-variable position of @(lhs rightRule)@ and
+-- @subst@ is a most general substitution with these properties.
+data CP f v v' = CP {
+    left :: Term f (Either v v'),  -- ^ left reduct
+    top :: Term f (Either v v'),   -- ^ source
+    right :: Term f (Either v v'), -- ^ right reduct
+    leftRule :: Rule f v,          -- ^ rule applied on left side
+    leftPos :: Pos,                -- ^ position of left rule application
+    rightRule :: Rule f v',        -- ^ rule applied on right side
+    subst :: Subst f (Either v v') -- ^ common substitution of the rewrite steps
+}
